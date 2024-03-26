a089e561d46c509067f98d0cb00b70b8595406df
afrimymeri <afrimymeri0@gmail.com>
2024-03-26 22:14:32 +0100

    efshiva statistics

--- contact.php
+++ contact.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MotorEmpire | Contact</title>
     <meta name="description" content="">
+    <link rel="icon" type="image/x-icon" href="images/favicon.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
         <link rel="stylesheet" href="bootstrap+fonte/bootstrap.min.css">
         <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
