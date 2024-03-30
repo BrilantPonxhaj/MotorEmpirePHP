817efd607130381b7f25e1808ea7fd659c75adee
afrimymeri <afrimymeri0@gmail.com>
2024-03-30 02:24:09 +0100

    lidhjamedatabaz

---
+++ database/configDatabase.php
@@ -0,0 +1,15 @@
+<?php 
+$DB_SERVER = "localhost:3308";
+$DB_USERNAME = "root";
+$DB_PASSWORD = "";
+$DB_NAME = "projekti";
+
+$conn = new mysqli($DB_SERVER, $DB_USERNAME, $DB_PASSWORD, $DB_NAME);
+
+if (mysqli_connect_errno()) {
+    echo "Falied to connect";
+    exit();
+}
+echo "Connected successfully";
+
+?>
