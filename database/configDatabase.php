1e0c29658dd23c3c6b1bfca5e82ebd5ba3d8f69b
Auronmussliu1 <auron.musliu@student.uni-pr.edu>
2024-05-22 12:01:56 +0200

    login fixed

--- database/configDatabase.php
+++ database/configDatabase.php
@@ -1,7 +1,7 @@
 <?php 
 $DB_SERVER = "localhost:3308";
 $DB_USERNAME = "root";
-$DB_PASSWORD = "Agnesa.010704";
+$DB_PASSWORD = "";
 $DB_NAME = "projekti";
 
 $conn = new mysqli($DB_SERVER, $DB_USERNAME, $DB_PASSWORD, $DB_NAME);
@@ -9,22 +9,6 @@ $conn = new mysqli($DB_SERVER, $DB_USERNAME, $DB_PASSWORD, $DB_NAME);
 if (mysqli_connect_errno()) {
     echo "Falied to connect";
     exit();
-}
-
-?>
-
-<?php 
-// MainDB database for cars data
-$servername = "localhost";
-$username = "root";
-$password = "Agnesa.010704"; 
-$dbname = "maindb";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
 }
 
 ?>
